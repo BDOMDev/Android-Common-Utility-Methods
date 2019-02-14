2f1f031895b2651c4a65f5dd00a91566bc78bea7
OshanDev <oshan@azbow.com>
2019-02-14 15:51:12 +0530

    first commit

---
+++ app/src/main/java/com/bdom/androidutilitymethods/BitmapUtils.java
@@ -0,0 +1,38 @@
+package com.bdom.androidutilitymethods;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+/**
+ * Created by Oshan on 2/14/19.
+ * Azbow Pvt. Ltd.
+ */
+public class BitmapUtils {
+
+    /**
+     * Apply monochrome color filter on the supplied bitmap.
+     *
+     * @param view The view to get bitmap from
+     * @param width Width of the required bitmap
+     * @param height Height of the required bitmap
+     *
+     * @return The new bitmap representing the view
+     */
+    public static Bitmap loadBitmapFromView(View view, int width, int height) {
+        Bitmap b = Bitmap.createBitmap(width , height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        view.layout(0, 0, view.getLayoutParams().width, view.getLayoutParams().height);
+        view.draw(c);
+        return b;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/bdom/androidutilitymethods/ScreenUtils.java
@@ -0,0 +1,75 @@
+package com.bdom.androidutilitymethods;
+
+
+/*
+ * Copyright (C) 2019 Oshan Madushanka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class ScreenUtils {
+
+    /**
+     * Returns the width of the screen
+     *
+     * @return int (pixels)
+     */
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * Returns the height of the screen
+     *
+     * @return int (pixels)
+     */
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * Returns the dp(Density-independent Pixels) value of the given px(Pixels) value
+     *
+     * @param context  the context
+     * @param px px value
+     *
+     * @return int (dp)
+     */
+    public static int dpFromPx(final Context context, final float px) {
+        return Math.round(px / context.getResources().getDisplayMetrics().density);
+    }
+
+    /**
+     * Returns the px(Pixels) value of the given dp(Density-independent Pixels) value
+     *
+     * @param context  the context
+     * @param dp dp value
+     *
+     * @return int (px)
+     */
+    public static int pxFromDp(final Context context, final float dp) {
+        return Math.round(dp * context.getResources().getDisplayMetrics().density);
+    }
+
+
+
+
+}
